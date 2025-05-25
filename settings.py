e0774ca31ea5ee9bc2ab39d1e3b92c4b80c708b5
mrechelon1 <139581772+mrechelon1@users.noreply.github.com>
2025-05-25 06:17:47 -0700

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+
+# Security settings
+SECRET_KEY = "3885cac067064bf3098f62854c211e68e78fcdc0abacf5935a39a509cec31964"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
